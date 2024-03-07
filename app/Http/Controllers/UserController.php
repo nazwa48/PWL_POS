1e4da7a76e480779f0e2a21e52bb94928f830024
nazwa48 <nazwaayunda48@gmail.com>
2024-03-07 17:35:44 +0700

    PRAK-2.2 JS-4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,10 +10,7 @@ class UserController extends Controller
 { 
     public function index() 
     {      
-        // coba akses model UserModel 
-        $user = UserModel :: findOr(20, ['username', 'nama'], function () { 
-            abort(404); 
-        }); 
-        return view('user', ['data' => $user]); 
+        $user = UserModel::where('username', 'manager9')->firstOrFail(); 
+        return view('user', ['data' => $user]);  
     } 
 } 
