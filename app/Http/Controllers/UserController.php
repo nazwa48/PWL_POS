09a6490a74375b56cdbf0f62c4efb9b587d3369c
nazwa48 <nazwaayunda48@gmail.com>
2024-03-12 23:35:38 +0700

    PRAK-2.7 JS-4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Hash;
  
 class UserController extends Controller 
 { 
-    public function index() 
-    {
-        $user = UserModel::all(); 
-        return view('user', ['data' => $user]); 
-    } 
+    // public function index() 
+    // {
+    //     $user = UserModel::all(); 
+    //     return view('user', ['data' => $user]); 
+    // } 
     public function tambah() 
     { 
         return view('user_tambah'); 
@@ -51,4 +51,9 @@ class UserController extends Controller
 
         return redirect('/user');
     }
+    public function index() 
+    { 
+        $user = UserModel::with('level')->get(); 
+        return view('user', ['data' => $user]); 
+    } 
 } 
