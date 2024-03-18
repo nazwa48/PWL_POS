81530ff8f7219bc907988ff7f332f25bd09cfb1a
nazwa48 <nazwaayunda48@gmail.com>
2024-03-18 14:36:47 +0700

    PRAK 123 JS-5

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -4,11 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\DataTables\KategoriDataTable;
+use App\Models\KategoriModel;
 
 class KategoriController extends Controller
 {
-    public function index()
+    public function index(KategoriDataTable $dataTable)
     {
+        return $dataTable->render('kategori.index');
+    }
+    public function create()
+    {
+        return view('kategori.create');
+    }
+
+    public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+        return redirect('/kategori');
+    }
+
         // $data = [
         // 'kategori_kode' => 'SNK',
         // 'kategori_nama' => 'Snack/Makanan Ringan',
@@ -17,18 +35,10 @@ class KategoriController extends Controller
         // DB::table('m_kategori') -> insert($data);
         // return 'insert data baru berhasil';
 
-
         // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
-        // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
+        // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row. ' baris';
 
-
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
-        // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row . ' baris';
-
-
-        $data = DB::table('m_kategori')->get();
-        return view('kategori' , ['data' => $data]);
-
-
-    }
+        // $data = DB::table('m_kategori')->get();
+        // return view('kategori', ['data' => $data]);
+    
 }
